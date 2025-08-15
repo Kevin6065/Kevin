a40621b8b7dcb0bdccaa75012e643fdbdc14087c
Kevin6065 <kevinyang0502@gmail.com>
2025-08-15 22:14:30 +0800

    8/15上課進度

---
+++ 09-SpringBootRest-Jpa/src/main/java/com/course/repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.course.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.course.entity.TodoEntity;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+	//JpaRepository<TodoEntity, Long>這邊是回傳TOdoEntity ,Long是Id的類型
+
+}
