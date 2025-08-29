363f755f883967ecadf02b3b0303279644fb94e4
Kevin6065 <kevinyang0502@gmail.com>
2025-08-29 21:55:15 +0800

    8/29上課進度

--- exp-ec-lab/src/main/java/com/course/dto/ProductDto.java
+++ exp-ec-lab/src/main/java/com/course/dto/ProductDto.java
@@ -33,6 +33,8 @@ public class ProductDto {
 	private String memo;
 	
 	private String cname;
+	
+	private Long productId;
 
 	public ProductDto() {
 
@@ -126,5 +128,13 @@ public class ProductDto {
 	public void setCname(String cname) {
 		this.cname = cname;
 	}
+	
+	
+	public Long getProductId() {
+			return productId; 
+	}
 
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
 }
